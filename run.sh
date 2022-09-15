bb1e28c5690fea9c5d27ab1e88990e52416a1e07
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-09-15 16:55:43 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 pip install -r /app/requirements.txt
 cd /app
 #ls
-#if [ -f requirements.txt ]; then
-#  #pip install -r requirements.txt
-#  echo "hi
-#fi
+if [ -f requirements.txt ]; then
+  #pip install -r requirements.txt
+  echo "hi"
+else
+  echo "hihi"
+fi
 #echo $COMMAND
 #cat /run.sh
 COMMAND="flask run -h 0.0.0.0 -p 5000"
