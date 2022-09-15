c75264a6dedc79eb72db6d4ee306b423c5a9f815
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-09-15 17:03:53 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-pip install -r /app/requirements.txt
+#pip install -r /app/requirements.txt
 cd /app
 #ls
 if [ -f requirements.txt ]; then
-  #pip install -r requirements.txt
-  echo "hi"
+  pip install -r requirements.txt
+  #echo "hi"
 else
-  echo "hihi"
+  :
 fi
 #echo $COMMAND
 #cat /run.sh
