7e7e2c29d9f44a021af4a634daf105f90096cb82
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-09-16 14:19:06 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,8 @@
 cd /app
 if [ -f requirements.txt ]; then
-  sudo -u user pip install -r requirements.txt > /dev/null 
-elif [ -f requirements.txt ] && [[ "$SUDO" == true ]]
-then
-  pip install -r requirements.txt > /dev/null
+  sudo -u user pip install -r requirements.txt > /dev/null
 else
   :
 fi
 COMMAND="flask run -h 0.0.0.0 -p 5000"
-if [ "$SUDO" = true ]
-then
-  $COMMAND
-else
-  sudo -u user $COMMAND
-fi
+eval $COMMAND
