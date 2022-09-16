4c298f137808f6efaed82c26085adca985126d2d
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-09-16 15:19:49 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
 cd /app
-whoami
 if [ -f requirements.txt ]; then
   PATH="${PATH}:/home/user/.local/bin"
   pip install -r requirements.txt > /dev/null
 else
   :
 fi
-COMMAND="sudo flask run -h 0.0.0.0 -p 5000"
-if [[ $COMMAND == *"sudo"* ]]; then
-  sudo PATH="${PATH}:/home/user/.local/bin"
-fi
+COMMAND="flask run -h 0.0.0.0 -p 5000"
 
 $COMMAND
